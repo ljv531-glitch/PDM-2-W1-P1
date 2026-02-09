800e64dcb45b3f65a72523aa0a26ad62a2923715
ljv531-glitch <ljv531@york.ac.uk>
2026-02-09 14:02:20 +0000

    Update sketch.js

--- 1star/sketch.js
+++ 1star/sketch.js
@@ -26,39 +26,46 @@ function fillArray(maxValue) {
  * @param {any[]} arr An array storing any type of element (e.g. numbers, strings)
  * @param {any} value The value to search for
  * @returns {boolean} True if the value is found, false if it is not.
- * @example
- * // returns false
- * search(["a", "b", "c"], 5);
- * // returns true
- * search(["a", "b", "c"], "c");
- * // returns true
- * search([5, -1, 9], 5);
  */
+function search(arr, value) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === value) {
+            return true;
+        }
+    }
+    return false;
+}
 
 
 /**
  * Gets the total of all items in a number array.
  * @param {number[]} arr 
  * @returns {number} The total of all items
- * @example
- * // returns 6
- * total([1, 2, 3]);
- * // returns 0
- * total([])
  */
+function total(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
 
 
 /**
  * Counts the number of times the given value appears in the array.
  * @param {any[]} arr The array to search.
- * @param {any[]} value The value to count.
+ * @param {any} value The value to count.
  * @returns {number} The number of times the value appears.
- * @example 
- * // returns 2
- * count([0, 25, 3, 4, 3], 3)
- * // returns 0
- * count([0, 35, 3, 4, 3], 7)
  */
+function count(arr, value) {
+    let counter = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === value) {
+            counter++;
+        }
+    }
+    return counter;
+}
 
 
 /**
@@ -67,9 +74,13 @@ function fillArray(maxValue) {
  * @param {any[]} arr The array to repeat 
  * @param {number} times The number of times to repeat the array values
  * @returns {any[]} A new array
- * @example
- * // returns [1, 2, 3, 1, 2, 3]
- * repeat([1, 2, 3], 2)
- * // returns [4, 4, 4]
- * repeat([4], 3)
  */
+function repeat(arr, times) {
+    const result = [];
+    for (let i = 0; i < times; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            result.push(arr[j]);
+        }
+    }
+    return result;
+}
